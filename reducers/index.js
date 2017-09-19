28420d7fc5dd6b843feea69cc7c0cc1ed761a7fc
Kamal Osman <kamaltmo@gmail.com>
2017-09-19 11:11:00 +0100

    created basic reducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,1 +1,24 @@
 //Reducers for default actions
+import actions from '../actions';
+import stateCreator from '../utility/default'
+
+export const reducer = (state = stateCreator(1), action) => {
+  switch (action.type) {
+    case 'FEED':
+      state.feed = true;
+      return state
+    case 'WATER':
+      state.watered = true;
+      return state
+    case 'FEEL_PAIN':
+      state.health = state.health - (5 * state.difficultyValue)
+      return state
+    case 'FEEL_JOY':
+      if (state.happieness < 4) {
+        state.happieness++;
+      }
+      return state
+    default:
+      return state
+    }
+}
